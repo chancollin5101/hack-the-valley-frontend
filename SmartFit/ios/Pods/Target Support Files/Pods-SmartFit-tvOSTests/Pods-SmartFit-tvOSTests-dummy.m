f1909072755cd3f46a46b9b88223c6c00768000d
Collin Chan <chancollin5101@gmail.com>
2020-02-15 11:43:19 -0500

    working template

---
+++ SmartFit/ios/Pods/Target Support Files/Pods-SmartFit-tvOSTests/Pods-SmartFit-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartFit_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartFit_tvOSTests
+@end
---
+++ SmartFit/ios/Pods/Target Support Files/Pods-SmartFitTests/Pods-SmartFitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartFitTests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartFitTests
+@end
